03129d694ca86836f9101d37f9b66540aa03fb9b
terrablue <102580937+terrablue@users.noreply.github.com>
2022-12-04 16:31:42 +0100

    add exit codes and colors to tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,37 @@
 #!/bin/sh
 
+ret=0
+passed=0
+failed=0
+
+green=32
+red=31
+
+color () {
+  echo -e "\x1b[$2m$1\x1b[0m"
+}
+
 for file in test/*; do
   if [ -d "$file" ]; then
-    ./flog $file/main.js
+    if ./flog $file/main.js | grep -q 'x'; then
+      ((failed=failed+1))
+      echo -n "failed "
+      color $file $red
+      ret=1;
+    else
+      ((passed=passed+1))
+      echo -n "passed "
+      color $file $green
+    fi
   fi
 done
+
+echo -e "\ntotals\n======"
+
+echo -n "passed "
+color $passed $green
+echo -n "failed "
+color $failed $red
+echo -e "out of $((passed+failed)) tests"
+
+exit $ret
--- test/assert.js
+++ test/assert.js
@@ -1,5 +1,1 @@
-const success = () => log("\x1b[32m.\x1b[0m");
-const failure = () => log("\x1b[31mx\x1b[0m");
-const assert = result => result ? success() : failure();
-
-export default assert;
+export default result => log(result ? "." : "x");
