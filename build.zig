9309cf4786960421a59a0def4498f30b5f7ac922
terrablue <102580937+terrablue@users.noreply.github.com>
2023-02-20 23:24:58 +0200

    add libflog

--- build.zig
+++ build.zig
@@ -13,6 +13,46 @@ pub fn build(b: *Builder) void {
         .target = target,
     });
 
+    const libflog = b.addStaticLibrary(.{
+        .name = "flog",
+        .target = target,
+        .optimize = optimize,
+    });
+    libflog.linkLibC();
+    libflog.linkLibrary(s4.artifact("s4"));
+    libflog.linkLibrary(libgitz.artifact("gitz"));
+    libflog.installLibraryHeaders(s4.artifact("s4"));
+    libflog.install();
+    libflog.installHeader("src/flog.h", "flog.h");
+    libflog.addCSourceFiles(&.{
+        "src/app.c",
+        "src/database.c",
+        "src/engine.c",
+        "src/file.c",
+        "src/flog.c",
+        "src/git.c",
+        "src/module.c",
+        "src/module-json.c",
+        "src/string.c",
+        "src/commands/base.c",
+        "src/commands/file.c",
+        "src/commands/help.c",
+        "src/commands/info.c",
+        "src/commands/install.c",
+        "src/commands/list.c",
+        "src/commands/script.c",
+        "src/commands/sync.c",
+        "src/commands/update.c",
+        "src/commands/with.c",
+    }, &.{
+        "-std=c11",
+        "-pedantic",
+        "-Wall",
+        "-W",
+        "-Wno-missing-field-initializers",
+        "-fno-sanitize=undefined",
+    });
+
     const flog = b.addExecutable(.{
         .name = "flog",
         .target = target,
